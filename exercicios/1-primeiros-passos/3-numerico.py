974c562612b8be9bd2cfcfab7161f576be674657
Guilherme-Pilgrim <guilherme10205@gmail.com>
2024-11-28 20:10:59 -0300

    Atualizar o 3-numerico.py

--- exercicios/1-primeiros-passos/3-numerico.py
+++ exercicios/1-primeiros-passos/3-numerico.py
@@ -1,11 +1,16 @@
 data_nascimento = '05-10-1976'
 idade_numerica = 46
 altura = 1.74
-
+peso = 87
 # Descubra o tipo de dado de cada variável acima
-
+print(type(data_nascimento))
+print(type(idade_numerica))
+print(type(altura))
 
 # Realize uma operação entre dados do tipo string e inteiro
+nome = 'guilherme'
+nome*5
 
 
 # Realize uma operação entre dados do tipo inteiro e float
+imc = print(peso/altura)
